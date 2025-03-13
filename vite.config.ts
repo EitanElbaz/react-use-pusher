d1d33240af843d6f43c6e3b19ae9605e7af4d304
Eitan Elbaz <eitan.elbaz@twenty7tec.com>
2025-03-13 08:58:26 +0000

    vite build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,39 @@
 import { defineConfig } from 'vite';
+import typescript from '@rollup/plugin-typescript';
+import path from 'node:path';
+import { typescriptPaths } from 'rollup-plugin-typescript-paths';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    manifest: true,
+    minify: true,
+    reportCompressedSize: true,
+    lib: {
+      entry: {
+        main: path.resolve('src/index.ts'),
+      },
+      fileName: (format, entryName) => `${entryName}.${format}.js`,
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      external: ['react', 'pusher-js'],
+      output: {
+        globals: {
+          react: 'React',
+        },
+      },
+      plugins: [
+        typescriptPaths({
+          preserveExtensions: true,
+        }),
+        typescript({
+          sourceMap: false,
+          declaration: true,
+          outDir: 'dist',
+        }),
+      ],
+    },
+  },
 });
